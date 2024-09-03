3d138c57e8c441d0347c8ec59c30727568b47ca1
abhishek-01k <abhishek@push.org>
2024-09-03 13:02:07 +0530

    Fixed the modal for the user options

--- frontend/app/defi/page.tsx
+++ frontend/app/defi/page.tsx
@@ -1,26 +1,47 @@
-"use client";
-import { useState, useEffect, useRef } from "react";
-import { useActiveAccount } from "thirdweb/react";
-import LiquidityChart from "@/components/LiquidityChart";
-import ChainStats from "@/components/ChainStats";
-import ActionModal from "@/components/ActionModal";
-import { createThirdwebClient, defineChain, getContract, readContract } from "thirdweb";
+"use client"
+
+import { useEffect, useRef, useState } from "react"
+import {
+  createThirdwebClient,
+  defineChain,
+  getContract,
+  readContract,
+} from "thirdweb"
+import { useActiveAccount } from "thirdweb/react"
+
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import { Button } from "@/components/ui/button"
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs"
+import ActionModal from "@/components/ActionModal"
+import ChainStats from "@/components/ChainStats"
+import LiquidityChart from "@/components/LiquidityChart"
+import UserOptions from "@/components/UserOptions"
 
 export default function Home() {
-  const [deposited, setDeposited] = useState(0);
-  const [borrowed, setBorrowed] = useState(0);
-  const [isModalOpen, setIsModalOpen] = useState(false);
-  const [action, setAction] = useState("");
-  const [chains, setChains] = useState([]);
+  const [deposited, setDeposited] = useState(0)
+  const [borrowed, setBorrowed] = useState(0)
+  const [isModalOpen, setIsModalOpen] = useState(false)
+  const [action, setAction] = useState("")
+  const [chains, setChains] = useState([])
 
-  const chainsRef = useRef([]);
-  const address = useActiveAccount();
+  const chainsRef = useRef([])
+  const address = useActiveAccount()
 
   useEffect(() => {
     if (address?.address) {
-      updateStats();
+      updateStats()
     }
-  }, [address]);
+  }, [address])
 
   useEffect(() => {
     const loadChains = async () => {
@@ -28,7 +49,7 @@ export default function Home() {
         {
           name: "Arbitrium Sepolia",
           logo: "/arbitrumsepolia.webp",
-          color: "rgb(50, 60, 150)",
+          color: "#323C96",
           chainID: 421614,
           rpc: "https://arbitrum-sepolia.blockpi.network/v1/rpc/",
           wormholeID: 10003,
@@ -64,100 +85,101 @@ export default function Home() {
           balance: "0",
           data: [],
         },
-      ];
+      ]
 
-      chainsRef.current = spokechainsData;
-      setChains(spokechainsData);
-    };
+      chainsRef.current = spokechainsData
+      setChains(spokechainsData)
+    }
 
-    loadChains();
-  }, []);
+    loadChains()
+  }, [])
 
   useEffect(() => {
     const fetchChainBalances = async () => {
-      const updatedChains = [...chainsRef.current];
+      const updatedChains = [...chainsRef.current]
 
       for (let i = 0; i < updatedChains.length; i++) {
-        const chain = updatedChains[i];
+        const chain = updatedChains[i]
         const client = createThirdwebClient({
           clientId: process.env.NEXT_PUBLIC_THIRDWEB_KEY!,
-        });
+        })
 
         const contract = getContract({
           client,
           chain: defineChain(chain.chainID),
           address: chain.tokenAddress,
-        });
+        })
 
         const balance = await readContract({
           contract,
           method: "function balanceOf(address) view returns (uint256)",
           params: [address?.address!],
-        });
+        })
 
-        console.log(balance, "balance");
+        console.log(balance, "balance")
 
-        const weiBalance = parseInt(balance.toString()) / 10 ** 18;
-        updatedChains[i].balance = weiBalance;
+        const weiBalance = parseInt(balance.toString()) / 10 ** 18
+        updatedChains[i].balance = weiBalance
       }
 
-      chainsRef.current = updatedChains;
-      setChains(updatedChains);
-    };
+      chainsRef.current = updatedChains
+      setChains(updatedChains)
+    }
 
     if (chains.length > 0) {
-      fetchChainBalances();
+      fetchChainBalances()
     }
-  }, [address]);
+  }, [address])
 
   const handleAction = (actionType) => {
-    setAction(actionType);
-    setIsModalOpen(true);
-  };
+    setAction(actionType)
+    setIsModalOpen(true)
+  }
 
   const updateStats = async () => {
     const client = createThirdwebClient({
       clientId: process.env.NEXT_PUBLIC_THIRDWEB_KEY!,
-    });
+    })
 
     const contract = getContract({
       client,
       chain: defineChain(11155111),
       address: "0x76414c98ee9AD3F776054f16A351831b71870Ff3",
-    });
+    })
 
     const borrowsdata = await readContract({
       contract,
       method: "function borrows(address) view returns (uint256)",
       params: [address?.address!],
-    });
+    })
 
     const depositdata = await readContract({
       contract,
       method: "function deposits(address) view returns (uint256)",
       params: [address?.address!],
-    });
+    })
 
-    setDeposited(Number(depositdata) / 10 ** 18);
-    setBorrowed(Number(borrowsdata) / 10 ** 18);
-  };
+    setDeposited(Number(depositdata) / 10 ** 18)
+    setBorrowed(Number(borrowsdata) / 10 ** 18)
+  }
 
   return (
     <div className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-4">MultiChainDefi</h1>
+      <h1 className="mb-4 text-3xl font-bold">MultiChainDefi</h1>
       <p className="mb-4">
-        Multichaindefi innovates DeFi by offering a multi-chain lending and borrowing protocol...
+        Multichaindefi innovates DeFi by offering a multi-chain lending and
+        borrowing protocol...
       </p>
-      <div className="flex justify-between mb-4">
-        <div>
-          <button onClick={() => handleAction("Deposit")} className="btn btn-secondary mr-3">Deposit</button>
-          <button onClick={() => handleAction("Withdraw")} className="btn btn-secondary mr-3">Withdraw</button>
-          <button onClick={() => handleAction("Borrow")} className="btn btn-secondary mr-3">Borrow</button>
-          <button onClick={() => handleAction("Repay")} className="btn btn-secondary mr-3">Repay</button>
-        </div>
+
+      <div className="mb-4 flex justify-between">
+        <UserOptions chains={chains} updateStats={updateStats} />
         <div className="flex">
-          <p className="mr-3">Deposited: <span>{deposited}</span></p>
-          <p>Borrowed: <span>{borrowed}</span></p>
+          <p className="mr-3">
+            Deposited: <span>{deposited}</span>
+          </p>
+          <p>
+            Borrowed: <span>{borrowed}</span>
+          </p>
         </div>
       </div>
       <div className="flex">
@@ -169,14 +191,14 @@ export default function Home() {
         </div>
       </div>
 
-      {isModalOpen && (
+      {/* {isModalOpen && (
         <ActionModal
           action={action}
           chains={chains}
           updateStats={updateStats}
           setIsModalOpen={setIsModalOpen}
         />
-      )}
+      )} */}
     </div>
-  );
+  )
 }
--- frontend/components/ActionModal.tsx
+++ frontend/components/ActionModal.tsx
@@ -1,143 +1,177 @@
-import { useState } from "react";
-import { ethers } from "ethers";
-import { createThirdwebClient, defineChain, getContract } from "thirdweb";
-import { prepareContractCall } from "thirdweb"
-import { useSendTransaction } from "thirdweb/react";
+import React, { useState } from "react"
+import { ethers } from "ethers"
+import {
+  createThirdwebClient,
+  defineChain,
+  getContract,
+  prepareContractCall,
+} from "thirdweb"
+import { useSendTransaction } from "thirdweb/react"
 
-export default function ActionModal({ action, chains, updateStats, setIsModalOpen }) {
-  const [amount, setAmount] = useState("");
-  const [selectedChain, setSelectedChain] = useState(null);
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "@/components/ui/alert-dialog"
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select"
 
-  const { mutate: sendTransaction } = useSendTransaction();
+import { Button } from "./ui/button"
+import { Input } from "./ui/input"
+import { Label } from "./ui/label"
+
+export default function ActionModal({ action, chains, updateStats }) {
+  const [amount, setAmount] = useState("")
+  const [selectedChain, setSelectedChain] = useState(null)
+
+  const { mutate: sendTransaction } = useSendTransaction()
 
   const client = createThirdwebClient({
-    clientId: process.env.NEXT_PUBLIC_THIRDWEB_KEY!
-  });
-
+    clientId: process.env.NEXT_PUBLIC_THIRDWEB_KEY!,
+  })
 
   console.log(selectedChain)
 
-
   const handleSubmit = async (e: any) => {
-    e.preventDefault();
-
-    if (!selectedChain || !amount) return;
+    console.log("Called submit")
+    if (!selectedChain || !amount) return
 
     try {
       // Assuming the contract functions are similar to what you had in JS
-      const weiAmount = ethers.parseEther(amount.toString());
+      const weiAmount = ethers.parseEther(amount.toString())
 
       if (action === "Deposit") {
-
         const contract = getContract({
           client,
           chain: defineChain(selectedChain.chainID),
-          address: selectedChain?.spokeAddress
-        });
+          address: selectedChain?.spokeAddress,
+        })
 
         const transaction = prepareContractCall({
           contract,
           method: "function deposit(uint256 amount)",
-          params: [weiAmount]
-        });
+          params: [weiAmount],
+        })
 
-        await sendTransaction(transaction);
-
+        await sendTransaction(transaction)
       } else if (action === "Withdraw") {
-
         const contract = getContract({
           client,
           chain: defineChain(selectedChain.chainID),
-          address: selectedChain?.spokeAddress
-        });
+          address: selectedChain?.spokeAddress,
+        })
 
         const transaction = await prepareContractCall({
           contract,
           method: "function requestWithdraw(uint256 amount)",
-          params: [weiAmount]
-        });
+          params: [weiAmount],
+        })
 
-        await sendTransaction(transaction);
-
+        await sendTransaction(transaction)
       } else if (action === "Borrow") {
-
         const contract = getContract({
           client,
           chain: defineChain(selectedChain.chainID),
-          address: selectedChain?.spokeAddress
-        });
+          address: selectedChain?.spokeAddress,
+        })
 
         const transaction = await prepareContractCall({
           contract,
           method: "function requestBorrow(uint256 amount)",
-          params: [weiAmount]
-        });
+          params: [weiAmount],
+        })
 
-        await sendTransaction(transaction);
-
+        await sendTransaction(transaction)
       } else if (action === "Repay") {
-
         const contract = getContract({
           client,
           chain: defineChain(selectedChain.chainID),
-          address: selectedChain?.spokeAddress
-        });
+          address: selectedChain?.spokeAddress,
+        })
 
         const transaction = await prepareContractCall({
           contract,
           method: "function repayBorrow(uint256 amount)",
-          params: [weiAmount]
-        });
+          params: [weiAmount],
+        })
 
-        await sendTransaction(transaction);
+        await sendTransaction(transaction)
       }
 
-      updateStats(weiAmount); // Update stats after the transaction
-      setIsModalOpen(false);
+      updateStats(weiAmount) // Update stats after the transaction
+      // setIsModalOpen(false);
     } catch (err) {
-      console.error(err);
+      console.error(err)
     }
-  };
-
-
+  }
 
   return (
-    <div className="modal">
-      <div className="modal-content">
-        <span className="close" onClick={() => setIsModalOpen(false)}>&times;</span>
-        <h2>{action}</h2>
-        <form onSubmit={(e) => handleSubmit(e)}>
-          <div className="mb-3">
-            <label htmlFor="amount">Amount</label>
-            <input
-              type="number"
-              className="form-control"
-              id="amount"
-              value={amount}
-              onChange={(e) => setAmount(e.target.value)}
-              placeholder="100"
-            />
-          </div>
-          <div className="mb-3">
-            <label htmlFor="chains-dropdown">Chain</label>
-            <select
-              className="form-select"
-              id="chains-dropdown"
-              value={selectedChain}
-              onChange={(e) => setSelectedChain(chains[e.target.value])}
-            >
-              {chains.map((chain, index) => (
-                <option key={index} value={index}>
-                  {chain.name}
-                </option>
-              ))}
-            </select>
-          </div>
-          <div className="d-grid mb-3">
-            <button className="btn btn-primary" type="submit">Execute</button>
-          </div>
-        </form>
-      </div>
-    </div>
-  );
+    <>
+      <AlertDialog>
+        <AlertDialogTrigger asChild>
+          <Button variant="outline">{action}</Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogHeader>
+            <AlertDialogTitle>{action}</AlertDialogTitle>
+            <AlertDialogDescription></AlertDialogDescription>
+          </AlertDialogHeader>
+          <form onSubmit={(e) => handleSubmit(e)}>
+            <div className="modal">
+              <div className="modal-content">
+                <div className="mb-3">
+                  <Label htmlFor="amount">Amount</Label>
+                  <Input
+                    type="number"
+                    className="form-control"
+                    id="amount"
+                    value={amount}
+                    onChange={(e) => setAmount(e.target.value)}
+                    placeholder="Enter amount"
+                  />
+                </div>
+                <div className="mb-3">
+                  <Label htmlFor="chains-dropdown">Chain</Label>
+                  <Select onValueChange={(value) => setSelectedChain(value)}>
+                    <SelectTrigger className="">
+                      <SelectValue placeholder="Select Chain" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Select Chain</SelectLabel>
+                        {chains.map((chain) => (
+                          <SelectItem value={chain.name}>
+                            {chain.name}
+                          </SelectItem>
+                        ))}
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                </div>
+              </div>
+            </div>
+            <AlertDialogFooter>
+              <AlertDialogCancel>Cancel</AlertDialogCancel>
+              <Button className="btn btn-primary" type="submit">
+                Execute
+              </Button>
+            </AlertDialogFooter>
+          </form>
+        </AlertDialogContent>
+      </AlertDialog>
+    </>
+  )
 }
